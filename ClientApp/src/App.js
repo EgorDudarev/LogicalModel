7fb911edc6e9436d329b151ad78556f20eba1cb4
e.e.dudarev <e.e.dudarev@yandex.ru>
2023-02-10 19:51:51 +0500

    Стартовая версия 0.0

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { Entities } from './components/Entities';
 
 import './custom.css'
@@ -14,10 +11,7 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-            <Route path='/entities' component={Entities} />
+            <Route exact path='/' component={Entities} />
       </Layout>
     );
   }
